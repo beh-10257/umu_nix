234bd1b72bb53a1baf4a7b8308c6e3a9e0c96740
beh-10257 <beh-10257@users.noreply.github.com>
2024-10-17 22:06:33 +0100

    maybe the right way to expose version

--- packaging/nix/combine.nix
+++ packaging/nix/combine.nix
@@ -1,9 +1,9 @@
-{ env, package, symlinkJoin, version ? "1.1.3" }:
+{ env, package, symlinkJoin,version }:
 symlinkJoin {
   name = "umu-run-bwrap";
   paths = [
     env
-    (package.override {version = version;})
+    (package.override {version = "${version}";})
   ];
   postBuild = ''
     rm $out/bin/umu
--- packaging/nix/flake.nix
+++ packaging/nix/flake.nix
@@ -10,11 +10,12 @@
   in
   let
   pyth = nixpk.pkgs.python3;
+  version = "1.1.3";
   in
   let
-    umu-package = nixpk.callPackage ./umu-launcher.nix { umu-launcher=umu-launcher-src; pyth1=pyth; };
+    umu-package = nixpk.callPackage ./umu-launcher.nix { umu-launcher=umu-launcher-src; pyth1=pyth; version = "${version}"; };
     umu-run = nixpk.callPackage ./umu-run.nix { package=umu-package; };
   in{
-    packages.x86_64-linux.umu = nixpk.callPackage ./combine.nix { env=umu-run; package=umu-package; version = "1.1.3"; };
+    packages.x86_64-linux.umu = nixpk.callPackage ./combine.nix { env=umu-run; package=umu-package; version = "${version}"; };
   };
 }
